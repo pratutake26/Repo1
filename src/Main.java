c56a7b6a2ced7b5ffda600bdc55b67018714763e
pratikshatake <pratikshatake131@gmail.com>
2023-02-22 23:06:26 +0530

    done

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a=10;
+        int b=10;
+        int c=a*b;
+        System.out.println(c);
+
+
+
+
+
+
+
+
+    }
+}
